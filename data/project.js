6f2e11d23857759c99d87ebbd189d73084b3cc8d
giscafer <giscafer@outlook.com>
2019-06-26 18:54:03 +0800

    update

---
+++ data/project.js
@@ -0,0 +1,124 @@
+var projectList = [
+  {
+    name: 'IPS',
+    date: '2017/04',
+    description: '智能调度系统',
+    url: 'https://core.1ziton.com'
+  },
+  {
+    name: 'CMP',
+    date: '2017/06',
+    description: '承运商管理系统',
+    url: 'https://cmp.1ziton.com'
+  },
+  {
+    name: 'BOSS',
+    date: '2017/09',
+    description: '财务结算系统',
+    url: 'https://boss.1ziton.com'
+  },
+  {
+    name: 'BMS',
+    date: '2017/08',
+    description: '商家管理平',
+    url: 'https://bms.1ziton.com'
+  },
+  {
+    name: '新家装通5.0',
+    date: '2017/06',
+    description: '家装通重构',
+    url: 'https://jzt.1ziton.com'
+  },
+  {
+    name: '直营IPS',
+    date: '2017/10',
+    description: '直营网点系统',
+    url: 'https://self.1ziton.com'
+  },
+  {
+    name: 'WMS2.0',
+    date: '2017/11',
+    description: '仓储管理系统',
+    url: 'https://wms.1ziton.com'
+  },
+  {
+    name: '官网2.0',
+    date: '2018/01',
+    description: '一智通官网重构，从PHP技术栈重构为Java技术栈',
+    url: 'https://www.1ziton.com'
+  },
+  {
+    name: '数据大屏',
+    date: '2018/04',
+    description: '数据大屏展示系统'
+  },
+  {
+    name: 'SCM2.0',
+    date: '2018/06',
+    description: 'SCM2.0，重构TMS桌面端到Web端，整合业务'
+  },
+  {
+    name: 'BMS2.0',
+    date: '2018/08',
+    description: 'BMS2.0前端改版，优化体验'
+  },
+  {
+    name: 'WMS3.0',
+    date: '2018/09',
+    description: 'WMS3.0启动开发'
+  },
+  {
+    name: 'WMS PDA',
+    date: '2018/09',
+    description: 'WMS PDA启动开发，App技术栈转为React Native'
+  },
+  {
+    name: '方米立临时官网',
+    date: '2018/12',
+    description: '一人一天半开发的工具网站（Node.js+MongoDB+LayUI）',
+    url: 'https://yanshuo.io/assets/player/?deck=5c6b5d5da91c9300548b8083#/'
+  },
+  {
+    name: '方米立小程序',
+    date: '2018/12',
+    description: '使用Taro开发方米立家居电商小程序'
+  },
+  {
+    name: '方米立运营端',
+    date: '2018/12',
+    description: '方米立运营端管理系统',
+    url: 'https://yunying.fangmili.com'
+  },
+  {
+    name: '数据平台系统',
+    date: '2018/10',
+    description: '数据平台2.0版本',
+    url: 'https://bigdata.1ziton.com'
+  },
+  {
+    name: '小智哥App',
+    date: '2019/04',
+    description: '小智哥 App'
+  },
+  {
+    name: '小智哥运营端',
+    date: '2019/04',
+    description: '小智哥运营管理系统',
+    url: 'https://xiaozhige.1ziton.com'
+  },
+  {
+    name: '方米立2.0',
+    date: '2019/05',
+    description: '分销小C端功能上线'
+  },
+  {
+    name: '前端CI/CD',
+    date: '2019/06',
+    description: '前端工程化建设，DevOps流程实践运行'
+  },
+  {
+    name: '继续前行',
+    date: '现在',
+    description: '系统是开发不完的…思考如何在实践过程中不断成长！'
+  }
+];
