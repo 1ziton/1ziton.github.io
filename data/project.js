65ed31f9d1cc4fa602aedead99a65f4d5505dc19
giscafer <giscafer@outlook.com>
2019-06-26 19:21:41 +0800

    chore: 修改细节

--- data/project.js
+++ data/project.js
@@ -1,76 +1,88 @@
 var projectList = [
   {
+    name: '团队组建',
+    date: '2017/03',
+    description: '2017年3月giscafer入职，前端开始组建团队，人数从1个增加到7个'
+  },
+  {
     name: 'IPS',
     date: '2017/04',
-    description: '智能调度系统',
+    description: '智能调度系统（5人+）',
     url: 'https://core.1ziton.com'
+  },
+  {
+    name: '大前端组',
+    date: '2017/05',
+    description:
+      'Web前端组与App开发组合并为大前端，技术栈Web为Angular，App为Ionic（APP开发3人，Web约7~9人）'
   },
   {
     name: 'CMP',
     date: '2017/06',
-    description: '承运商管理系统',
+    description: '承运商管理系统（2人）',
     url: 'https://cmp.1ziton.com'
   },
   {
     name: 'BOSS',
     date: '2017/09',
-    description: '财务结算系统',
+    description: '财务结算系统（3人）',
     url: 'https://boss.1ziton.com'
   },
   {
     name: 'BMS',
     date: '2017/08',
-    description: '商家管理平',
+    description: '商家管理平台（3人）',
     url: 'https://bms.1ziton.com'
   },
   {
     name: '新家装通5.0',
     date: '2017/06',
-    description: '家装通重构',
+    description: '家装通重构（6人，21天上线）',
     url: 'https://jzt.1ziton.com'
   },
   {
     name: '直营IPS',
     date: '2017/10',
-    description: '直营网点系统',
+    description: '直营网点系统（2人）',
     url: 'https://self.1ziton.com'
   },
   {
     name: 'WMS2.0',
     date: '2017/11',
-    description: '仓储管理系统',
+    description: '仓储管理系统（2人+）',
     url: 'https://wms.1ziton.com'
   },
   {
     name: '官网2.0',
     date: '2018/01',
-    description: '一智通官网重构，从PHP技术栈重构为Java技术栈',
+    description:
+      '一智通官网重构，从PHP技术栈重构为Java技术栈，前端用ng1.x，为了兼容老浏览器（3人+）',
     url: 'https://www.1ziton.com'
   },
   {
     name: '数据大屏',
     date: '2018/04',
-    description: '数据大屏展示系统'
+    description: '数据大屏展示系统（1人）'
   },
   {
     name: 'SCM2.0',
     date: '2018/06',
-    description: 'SCM2.0，重构TMS桌面端到Web端，整合业务'
+    description: 'SCM2.0，重构TMS桌面端到Web端，整合业务（2~4人浮动）'
   },
   {
     name: 'BMS2.0',
     date: '2018/08',
-    description: 'BMS2.0前端改版，优化体验'
+    description: 'BMS2.0前端改版，优化体验（3人）'
   },
   {
     name: 'WMS3.0',
     date: '2018/09',
-    description: 'WMS3.0启动开发'
+    description: 'WMS3.0启动开发（3人）'
   },
   {
     name: 'WMS PDA',
     date: '2018/09',
-    description: 'WMS PDA启动开发，App技术栈转为React Native'
+    description: 'WMS PDA启动开发，App技术栈转为React Native，鸡哥一人独揽'
   },
   {
     name: '方米立临时官网',
@@ -81,40 +93,42 @@ var projectList = [
   {
     name: '方米立小程序',
     date: '2018/12',
-    description: '使用Taro开发方米立家居电商小程序'
+    description: '使用Taro开发方米立家居电商小程序（2人）'
   },
   {
     name: '方米立运营端',
     date: '2018/12',
-    description: '方米立运营端管理系统',
+    description: '方米立运营端管理系统（2人）',
     url: 'https://yunying.fangmili.com'
   },
   {
     name: '数据平台系统',
     date: '2018/10',
-    description: '数据平台2.0版本',
+    description: '数据平台2.0版本（1人）',
     url: 'https://bigdata.1ziton.com'
   },
   {
     name: '小智哥App',
     date: '2019/04',
-    description: '小智哥 App'
+    description: '小智哥 App （React Native技术栈，2~3人）'
   },
   {
     name: '小智哥运营端',
     date: '2019/04',
-    description: '小智哥运营管理系统',
+    description: '小智哥运营管理系统，基本是大罗（妹子）一人独揽',
     url: 'https://xiaozhige.1ziton.com'
   },
   {
     name: '方米立2.0',
     date: '2019/05',
-    description: '分销小C端功能上线'
+    description: '分销小C端功能上线（1人）'
   },
   {
     name: '前端CI/CD',
     date: '2019/06',
-    description: '前端工程化建设，DevOps流程实践运行'
+    description:
+      '前端工程化建设，DevOps流程实践运行（giscafer独自一人，运维配合）',
+    url: 'https://github.com/giscafer/front-end-manual/issues/26'
   },
   {
     name: '继续前行',
